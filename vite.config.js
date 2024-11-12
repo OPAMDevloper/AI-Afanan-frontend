0003126bd71e2f4d2dfff5931d2972b3e8e9aa78
swapnilkag <opamdeveloper@gmail.com>
2024-11-12 22:19:06 +0530

    change host

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'import.meta.env.VITE_APP_HOST': JSON.stringify(process.env.VITE_APP_HOST),
+    'import.meta.env.VITE_APP_PORT': JSON.stringify(process.env.VITE_APP_PORT),
+  },
   plugins: [react()],
   resolve: {
     alias: {
@@ -11,7 +15,7 @@ export default defineConfig({
 
     server: {
       port: Number(process.env.VITE_APP_PORT) || 3004,
-      host: process.env.VITE_APP_HOST || 'localhost'
+      host: process.env.VITE_APP_HOST || '0.0.0.0'
     },
   },
 });
