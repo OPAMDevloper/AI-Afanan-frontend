dde48430ed15c69ad42f4671aed94acc8db63f6a
Burhan <jariwala@genius-germany.de>
2024-11-30 18:17:54 +0530

    fix: footer code

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,40 @@
+import React, { useCallback, useContext, useEffect } from 'react';
 import './Footer.css'; // Make sure to update your CSS
 import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram } from 'react-icons/fa';
+import { StoreContext } from '../../context/storeContext';
+import axios from 'axios';
 
 const Footer = () => {
+  const [categories, setCategories] = React.useState([]);
+  const { url ,token} = useContext(StoreContext);
+
+  
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = useCallback(async () => {
+    try {
+      console.log(token,'token');
+      
+      const response = await axios.get(`${url}/category`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setCategories(response.data.data || []);
+      if (response.data.data.length > 0) {
+        setSelectedCategory(response.data.data[0]); // Select the first category by default
+      }
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  }, [url,token]);
+
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        <div className="footer-subscribe">
+        <div className="footer-subscribes">
           <h2>Local Face</h2>
           <p>Subscribe to Our Newsletter:</p>
           <p>Receive Updates on New Arrivals and Special Promotions!</p>
@@ -25,38 +54,34 @@ const Footer = () => {
           <div className="footer-column">
             <h3>Categories</h3>
             <ul>
-              <li><a href="#">Fashion</a></li>
-              <li><a href="#">Jewelry</a></li>
-              <li><a href="#">Sports</a></li>
-              <li><a href="#">Electronics</a></li>
-              <li><a href="#">Indoor</a></li>
+            {categories.map((category, index) => (
+              <li><a key={index} href="/shop">{category.name}</a></li>
+            ))}
+
             </ul>
           </div>
-          <div className="footer-column">
+          {/* <div className="footer-column">
             <h3>Shopping</h3>
             <ul>
               <li><a href="#">Payments</a></li>
               <li><a href="#">Delivery options</a></li>
               <li><a href="#">Buyer protection</a></li>
             </ul>
-          </div>
+          </div> */}
           <div className="footer-column">
             <h3>Customer care</h3>
             <ul>
-              <li><a href="#">Help center</a></li>
-              <li><a href="#">Terms & Conditions</a></li>
-              <li><a href="#">Privacy policy</a></li>
-              <li><a href="#">Returns & refund</a></li>
-              <li><a href="#">Survey & feedback</a></li>
+              <li><a href="#">+1 9999 9999</a></li>
+              <li><a href="#">perfumes@gmail.com</a></li>
             </ul>
           </div>
           <div className="footer-column">
             <h3>Pages</h3>
             <ul>
+              <li><a href="/">Home</a></li>
               <li><a href="/About">About Us</a></li>
               <li><a href="/Shop">Shop</a></li>
-              <li><a href="#">Contact Us</a></li>
-              <li><a href="#">Services</a></li>
+              {/* <li><a href="#">Contact Us</a></li> */}
               <li><a href="/Blog">Blog</a></li>
             </ul>
           </div>
