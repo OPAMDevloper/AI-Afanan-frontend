fa92a3bcecc1823be065a3f0386423676a257aed
Burhan <jariwala@genius-germany.de>
2024-12-22 20:07:59 +0530

    fix url

--- src/context/storeContext.jsx
+++ src/context/storeContext.jsx
@@ -23,7 +23,7 @@ const deleteCookie = (name) => {
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const url = 'https://46.202.176.129:3002';
+  const url = 'https://alafnanperfume.com';
   const [token, setToken] = useState("");
   const [cartItems, setCartItems] = useState({});
   const [isDrawerOpen, setIsDrawerOpen] = useState(false); // Drawer state
